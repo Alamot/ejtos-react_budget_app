27f74ea614c66700adb3c090b61c17cba253a4b6
alamot <antalamot@gmail.com>
2023-02-11 16:52:15 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import ExpenseTotal from './components/ExpenseTotal';
 import ExpenseList from './components/ExpenseList';
 import AllocationForm from './components/AllocationForm';
 import RemainingBudget from './components/Remaining';
+import Currency from './components/Currency';
 
 import { AppProvider } from './context/AppContext';
 const App = () => {
@@ -16,15 +17,10 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                 <div className='row mt-3'>
-                    <div className='col-sm'>
-                        <Budget />
-                    </div>
-                    <div className='col-sm'>
-                        <RemainingBudget />
-                    </div>
-                    <div className='col-sm'>
-                        <ExpenseTotal />
-                    </div>
+                    <div className='col-sm'><Budget /></div>
+                    <div className='col-sm'><RemainingBudget /></div>
+                    <div className='col-sm'><ExpenseTotal /></div>
+                    <div className='col-sm'><Currency /></div>
                 </div>
                 <h3 className='mt-3'>Allocation</h3>
                 <div className='row '>
